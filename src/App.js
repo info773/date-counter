8aa221943d2ade84c14b95a1f89debf215f59dd4
Berry <info@felix-behrens.de>
2025-10-26 11:14:02 +0100

    Finished Project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Counter />;
 }
 
 export default App;
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const modDate = `${day}.${month}.${year}`;
+
+  function handleStepMinus() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  return (
+    <div className="container">
+      <div>
+        <button onClick={() => handleStepMinus()}> - </button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}> + </button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}> - </button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}> + </button>
+      </div>
+      <p>
+        {count === 0
+          ? `today is `
+          : `${count < 0 ? count * -1 : count}
+        ${count === 1 || count === -1 ? " day " : " days "}
+        ${count >= 0 ? "from today is" : " ago was"} `}
+        {modDate}
+      </p>
+    </div>
+  );
+}
